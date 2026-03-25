a2710d70d0fec1902a40170b929a00ccd3b960ea
KUSHAL-HC <kushalhcfrom1974@gmail.com>
2026-03-25 09:35:44 +0530

    all routing in the NavBar done successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/Contact" element={<Contact/>} />
+      </Routes>
     </>
   )
 }
