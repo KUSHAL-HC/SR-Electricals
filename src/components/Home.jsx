162edb70dc58e37d616d6ec27f85aed168b0e4b1
KUSHAL-HC <kushalhcfrom1974@gmail.com>
2026-03-27 08:16:45 +0530

    added hero slides of 3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,21 +15,50 @@ const Home = () => {
           Book A Site Visit
         </div>
       </div>
-      <div className="position-relative text-center text-white">
-        <img 
-          src={heroImage}
-          className="w-100" 
-          alt="hero"
-          style={{ filter: "brightness(50%)", maxHeight: "600px", objectFit: "cover" }}
-        />
+      
+      <div id="heroCarousel" className="carousel slide" data-bs-ride="carousel">
 
-        <div className="position-absolute top-50 start-50 translate-middle">
-          <div className="border-start border-5 border-warning ps-3">
-            <h1 className="display-4">Spares & Services</h1>
-            <p>Your trusted partner for reliable power equipment maintenance</p>
+            <div className="carousel-indicators">
+                  <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="0" className="active"></button>
+                  <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="1"></button>
+                  <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="2"></button>
+            </div>
+
+            <div className="carousel-inner">
+
+              <div className="carousel-item active">
+                  <img src={heroImage} className="d-block w-100" alt="slide1"  style={{ height: "600px", objectFit: "cover", filter: "brightness(50%)" }}/>
+                  <div className="carousel-caption">
+                      <h1>Spares & Services</h1>
+                      <p>Your trusted partner for maintenance</p>
+                  </div>
+              </div>
+
+              <div className="carousel-item">
+                  <img src={heroImage} className="d-block w-100" alt="slide2" style={{ height: "600px", objectFit: "cover", filter: "brightness(50%)" }}/>
+                  <div className="carousel-caption">
+                      <h1>Reliable Solutions</h1>
+                      <p>We deliver quality service</p>
+                  </div>
+              </div>
+
+              <div className="carousel-item">
+                  <img src={heroImage} className="d-block w-100" alt="slide3" style={{ height: "600px", objectFit: "cover", filter: "brightness(50%)" }}/>
+                  <div className="carousel-caption">
+                    <h1>Expert Engineers</h1>
+                    <p>Professional & experienced team</p>
+                  </div>
+              </div>
+              </div>
+
+            <button className="carousel-control-prev" type="button" data-bs-target="#heroCarousel" data-bs-slide="prev">
+                 <span className="carousel-control-prev-icon"></span>
+            </button>
+
+            <button className="carousel-control-next" type="button" data-bs-target="#heroCarousel" data-bs-slide="next">
+              <span className="carousel-control-next-icon"></span>
+            </button>
           </div>
-        </div>
-      </div>
 
 
       <div className="container bg-danger text-white text-center p-5 shadow position-relative z-100" style={{ marginTop: "-50px" }}>
